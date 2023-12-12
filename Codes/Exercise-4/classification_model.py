14713623bf435a0d6a4e5c0b5e657903f40f58f7
İbrahim Furkan TEZCAN <58287532+1-Kullanici@users.noreply.github.com>
2023-12-12 23:41:42 +0000

    Last change before 00:00.

--- Codes/Exercise-4/classification_model.py
+++ Codes/Exercise-4/classification_model.py
@@ -1,75 +1,140 @@
-import numpy as np
-import pandas as pd
-from sklearn import preprocessing
 import data_manipulation as dm
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, mean_absolute_error, r2_score
-from sklearn.metrics import roc_curve, roc_auc_score
+from sklearn.metrics import accuracy_score, confusion_matrix, mean_absolute_error, r2_score
+from sklearn.metrics import roc_curve
 import matplotlib.pyplot as plt
-import seaborn as sns
+
+
+
+def model_grader(y_test, prediction):
+    """Returns the accuracy, mean absolute error, r2 score and confusion matrix of the model"""
+    acc              = accuracy_score(y_test,        prediction)
+    mae              = mean_absolute_error(y_test,   prediction)
+    r2s              = r2_score(y_test,              prediction)
+    conf_matrix      = confusion_matrix(y_test,      prediction)
+    return acc, mae, r2s, conf_matrix
 
 
 if __name__ == '__main__':
 
     path = 'Classification Datasets/'
     files = dm.parse_files(path)
-    results = {}
+    results = []
+    filenames = []
 
     for file in files:
         data             = dm.read_data(file)
         data             = dm.null_separator(data)
-        target           = len(data.columns) - 1
-        print(target)
-         
         train,   test    = dm.split_data(data,    0.8)
+
+        target           = len(data.columns) - 1
         x_train, y_train = dm.data_divider(train, target)
-        print(x_train.shape)
-        print(y_train.shape)
         x_test,  y_test  = dm.data_divider(test,  target)
         filename         = file.split('/')[-1].removesuffix('.csv')
+        filenames.append(filename)
     
-        model_1            = LogisticRegression().fit(x_train, y_train)
-        prediction_1       = LogisticRegression.predict(model_1, x_test)
 
-        acc_1              = accuracy_score(test['class'],        prediction_1)
-        mae_1              = mean_absolute_error(test['class'],   prediction_1)
-        r2s_1              = r2_score(test['class'],              prediction_1)
-        conf_matrix_1      = confusion_matrix(test['class'],      prediction_1)
+        classifier_1     = LogisticRegression()
+        model_1          = classifier_1.fit(x_train, y_train)
+        prediction_1     = classifier_1.predict(model_1, x_test) # This does not work. LinearClassifierMixin.predict() takes 2 positional arguments but 3 were given. HOW???
 
-        results[str(model_1) + filename] = (acc_1, mae_1, r2s_1, conf_matrix_1)
+        classifier_2     = SVC()
+        model_2          = classifier_2.fit(x_train, y_train)
+        prediction_2     = classifier_2.predict(model_2, x_test)
 
-
-        model_2            = SVC().fit(x_train, y_train)
-        prediction_2       = SVC.predict(model_2, x_test)
-
-        acc_2              = accuracy_score(test['class'],        prediction_2)
-        mae_2              = mean_absolute_error(test['class'],   prediction_2)
-        r2s_2              = r2_score(test['class'],              prediction_2)
-        conf_matrix_2      = confusion_matrix(test['class'],      prediction_2)
-
-        results[str(model_2) + filename] = (acc_2, mae_2, r2s_2, conf_matrix_2)
+        classifier_3     = DecisionTreeClassifier()
+        model_3          = classifier_3.fit(x_train, y_train)
+        prediction_3     = classifier_3.predict(model_3, x_test)
 
 
-        model_3            = DecisionTreeClassifier().fit(x_train, y_train)
-        prediction_3       = DecisionTreeClassifier.predict(model_3, x_test)
+        results.append((y_test, prediction_1, prediction_2, prediction_3))
 
-        acc_3              = accuracy_score(test['class'],        prediction_3)
-        mae_3              = mean_absolute_error(test['class'],   prediction_3)
-        r2s_3              = r2_score(test['class'],              prediction_3)
-        conf_matrix_3      = confusion_matrix(test['class'],      prediction_3)
-        
-        results[str(model_3) + filename] = (acc_3, mae_3, r2s_3, conf_matrix_3)
 
-        print(results)
+    # ROC Curve
+    fpr = []
+    tpr = []
+    thresholds = []
+    scores = []
+    conf_matrices = []
+    for i in range(len(results)):
+        y_test, prediction_1, prediction_2, prediction_3 = results[i]
+        # fpr_1, tpr_1, thresholds_1 = roc_curve(y_test, prediction_1)
+        # fpr_2, tpr_2, thresholds_2 = roc_curve(y_test, prediction_2)
+        # fpr_3, tpr_3, thresholds_3 = roc_curve(y_test, prediction_3)
 
-        fpr, tpr, thresholds = roc_curve(test['class'], prediction_1)
-        plt.plot(fpr, tpr)
-        plt.xlabel('False Positive Rate')
-        plt.ylabel('True Positive Rate')
-        plt.title('ROC Curve')
-        plt.show()
+        # fpr.append([fpr_1, fpr_2, fpr_3])
+        # tpr.append([tpr_1, tpr_2, tpr_3])
+        # thresholds.append([thresholds_1, thresholds_2, thresholds_3])
+
+        acc_1, mae_1, r2s_1, conf_matrix_1 = model_grader(y_test, prediction_1)
+        acc_2, mae_2, r2s_2, conf_matrix_2 = model_grader(y_test, prediction_2)
+        acc_3, mae_3, r2s_3, conf_matrix_3 = model_grader(y_test, prediction_3)
+
+        print('Accuracy of Logistic Regression: ', acc_1)
+        print('Accuracy of SVM: ', acc_2)
+        print('Accuracy of Decision Tree: ', acc_3)
+
+        print('Mean Absolute Error of Logistic Regression: ', mae_1)
+        print('Mean Absolute Error of SVM: ', mae_2)
+        print('Mean Absolute Error of Decision Tree: ', mae_3)
+
+        print('R2 Score of Logistic Regression: ', r2s_1)
+        print('R2 Score of SVM: ', r2s_2)
+        print('R2 Score of Decision Tree: ', r2s_3)
+
+        scores.append(([acc_1, mae_1, r2s_1], [acc_2, mae_2, r2s_2], [acc_3, mae_3, r2s_3]))
+        conf_matrices.append([conf_matrix_1, conf_matrix_2, conf_matrix_3])
+
+
+
+    fig, [[ax1, ax2, ax3], [ax4, ax5, ax6]] = plt.subplots(figsize=(3, 3), 
+                                                            facecolor='lightskyblue',
+                                                            layout='constrained',
+                                                            nrows=2,
+                                                            ncols=3)
+
+
+    ax1.plot(conf_matrices[0,0], label=str(filenames[0]) + ' - Logistic Regression')
+    ax1.xlabel('False Positive Rate')
+    ax1.ylabel('True Positive Rate')
+    ax1.title(str(filenames[0]) + ' - Logistic Regression')
+
+    ax2.plot(conf_matrices[0,1], label=str(filenames[0]) + ' - SVC')
+    ax2.xlabel('False Positive Rate')
+    ax2.ylabel('True Positive Rate')
+    ax2.title(str(filenames[0]) + ' - SVC')
+
+    ax3.plot(conf_matrices[0,2], label=str(filenames[0]) + ' - Decision tree')
+    ax3.xlabel('False Positive Rate')
+    ax3.ylabel('True Positive Rate')
+    ax3.title(str(filenames[0]) + ' - Decision tree')
+    
+    ax4.plot(conf_matrices[1,0], label=str(filenames[1]) + ' - Logistic Regression')
+    ax4.xlabel('False Positive Rate')
+    ax4.ylabel('True Positive Rate')
+    ax4.title(str(filenames[1]) + ' - Logistic Regression')
+
+    ax5.plot(conf_matrices[1,1], label=str(filenames[1]) + ' - SVC')
+    ax5.xlabel('False Positive Rate')
+    ax5.ylabel('True Positive Rate')
+    ax5.title(str(filenames[1]) + ' - SVC')
+
+    ax6.plot(conf_matrices[1,2], label=str(filenames[1]) + ' - Decision tree')
+    ax6.xlabel('False Positive Rate')
+    ax6.ylabel('True Positive Rate')
+    ax6.title(str(filenames[1]) + ' - Decision tree')
+
+    # ax7.plot(fpr[0,0], tpr[0,0], label=str(filenames[0]) + ' - Logistic Regression')
+    # ax7.plot(fpr[0,1], tpr[0,1], label=str(filenames[0]) + ' - SVC')
+    # ax7.plot(fpr[0,2], tpr[0,2], label=str(filenames[0]) + ' - Decision Tree')
+    # ax7.title('ROC Curve')
+    # ax7.legend()
+
+    fig.show()
+
+    fig.savefig('Codes/Exercise-4/Results.png')
 
         # dm.save_data(train, path + filename + '_train')
         # dm.save_data(test,  path + filename + '_test')
